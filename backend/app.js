58a690241598f1a0beb5fa5305cc435ed5510407
Liban <LibanAbdulahi0008@gmail.com>
2024-12-13 19:59:43 -0800

    fle

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path');
 const { db } = require('./config/mongoose')
-app.use(cors({ origin: 'http://localhost:4000', credentials: true }))
+app.use(cors({ origin: 'https://mern-full-stack-ecommerce.onrender.com', credentials: true }))
 app.use(express.json({ limit: '20mb' }))
 app.use('/api', require('./routes/allpathroutes'));
-app.listen(3000, db, () => console.log('listen'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+})
+app.listen(4000, db, () => console.log('listen'));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4000,
+    port: 'https://mern-full-stack-ecommerce.onrender.com',
     // proxy: {
     //   '/api': {
     //     target: apiUrl,
